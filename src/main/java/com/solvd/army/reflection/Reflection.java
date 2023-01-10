7d1be256eee0bc4327428a56ea70e590babb73ee
Eric Liu <ericliu101400@gmail.com>
2023-01-09 21:08:21 -0600

    Rewrote Reflection

--- src/main/java/com/solvd/army/reflection/Reflection.java
+++ src/main/java/com/solvd/army/reflection/Reflection.java
@@ -1,19 +1,37 @@
 package com.solvd.army.reflection;
 
 import com.solvd.army.Runner;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.lang.reflect.Field;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
-import java.lang.reflect.Modifier;
+import java.lang.reflect.*;
+import java.util.logging.Logger;
 
-public class Reflection<T> {
-    private static Logger logger = LogManager.getLogger(Runner.class.getName());
-    private T army;
-    Class armyRanks = Class.forName("com.solvd.army.position.ArmyRanks");
-    String name = armyRanks.getName();
-    int modifier = armyRanks.getModifiers();
-    String convert = Modifier.toString(modifier);
+public class Reflection {
+    public static void main(String[] args) throws ClassNotFoundException, SecurityException, IllegalArgumentException  {
+        java.util.logging.Logger logger = Logger.getLogger(Runner.class.getName());
 
-    public Reflection() throws ClassNotFoundException {
+        Class armyClass = Class.forName("com.solvd.army.position.Captain");
+
+        Field[] fields = armyClass.getDeclaredFields();
+        for (Field field : fields) {
+            Class fieldType = field.getType();
+            String fieldName = field.getName();
+            logger.info(fieldType + " " + fieldName);
+        }
+
+        Constructor[] constructors = armyClass.getDeclaredConstructors();
+        for (Constructor constructor : constructors) {
+            String constructorName = constructor.getName();
+            logger.info(constructorName);
+        }
+
+        Method[] methods = armyClass.getDeclaredMethods();
+        for (Method method : methods) {
+            Class returnType = method.getReturnType();
+            String methodName = method.getName();
+            logger.info(returnType + " " + methodName);
+        }
     }
 }
